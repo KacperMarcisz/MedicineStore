9633dfa6d8d192d86f5fd6eb96763035d5f296c7
Kacper Marcisz <36709401+KacperMarcisz@users.noreply.github.com>
2019-04-03 09:53:18 +0200

    init database

---
+++ MedicineStore.API/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿
+namespace MedicineStore.API.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+            
+        }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
